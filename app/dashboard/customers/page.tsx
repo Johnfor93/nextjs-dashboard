1aed2c7ef0d09433030843f78e0263095c0b06c2
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2023-11-14 15:23:42 +0700

    Add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,28 @@
 import { Metadata } from "next";
+import Search from "@/app/ui/search";
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  // const customers = await fetchFilteredCustomers();
+  // return <h1>Hello Customers Page</h1>;
+  const query = searchParams?.query || "";
+
+  const totalPages = await fetchFilteredCustomers(query);
+  return (
+    <main>
+      <CustomersTable customers={totalPages} />
+    </main>
+  );
 }
